b829b024ba7d6e3d0b83ddb1fa5f9cff130ec12e
Otiel <rouillg@gmail.com>
2013-04-18 10:37:24 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+DeleteFileWhenCreated
+=====================
+
+Description
+-----------
+
+_DeleteFileWhenCreated_ is a command line application written in C# for Windows that can be used to delete a file as soon as it is created.
+
+_DeleteFileWhenCreated_ registers a file watcher to detect when the file is created, and deletes it instantly. The application runs in the background and can only be stopped through the Task Manager.
+
+Usage
+-----
+
+1. If you simply don't care about the sources, [download](https://github.com/Otiel/DeleteFileWhenCreated/archive/master.zip) the repository as a zip file and keep only the `Latest version` folder.
+2. Open a command line (Win+R -> cmd).
+2. Run `"C:\[path to exe]\Latest version\DeleteFileWhenCreated.exe" "C:\[path to file]\myFileToDelete"`.
+
+Bugs/Ideas
+----------
+
+If you have a bug to report, or simply an idea for an improvement or a new feature, please add them in the [issue tracker](https://github.com/Otiel/DeleteFileWhenCreated/issues).
+
+License
+-------
+
+_DeleteFileWhenCreated_ is licensed under the [WTFPL](http://www.wtfpl.net/) ![WTFPL icon](http://i.imgur.com/AsWaQQl.png).
---
+++ Sources/DeleteFileWhenCreated/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DeleteFileWhenCreated {
+
+    static class Program {
+
+        /// <summary>
+        /// The file to delete.
+        /// </summary>
+        private static String fileToDelete;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args) {
+            // For debug purposes
+            //args = new String[] { @"C:\Users\Public\Desktop\New Text Document.txt" };
+
+            if (args.Length != 1) {
+                return;
+            }
+
+            // Fetch arg
+            fileToDelete = args[0];
+            String folderToWatch = Path.GetDirectoryName(fileToDelete);
+
+            // Create a new FileSystemWatcher and set its properties
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = folderToWatch;
+            // Watch for changes in LastAccess and LastWrite times, and the renaming of files or 
+            // directories
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite |
+                NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+            // Add event handlers
+            //watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            //watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            //watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            // Begin watching
+            watcher.EnableRaisingEvents = true;
+
+            if (File.Exists(fileToDelete)) {
+                File.Delete(fileToDelete);
+            }
+
+            // Run indefinitely
+            Application.Run();
+        }
+
+        /// <summary>
+        /// Called when a watched file is created.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="e">The <see cref="FileSystemEventArgs"/> instance containing the event 
+        /// data.</param>
+        private static void OnCreated(object source, FileSystemEventArgs e) {
+            try {
+                if (File.Exists(fileToDelete)) {
+                    File.Delete(fileToDelete);
+                }
+            } catch {
+                // Exception occured, probably because the file is locked by another process
+
+                // Wait 1 sec
+                Thread.Sleep(1000);
+                // ... then retry to delete
+                if (File.Exists(fileToDelete)) {
+                    File.Delete(fileToDelete);
+                }
+            }
+        }
+    }
+}
